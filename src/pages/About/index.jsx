9820acf2dcb63bf7503df4ce64028467b1d4aefe
Saif-Arain786 <arainsaif06@gmail.com>
2025-03-01 17:23:59 +0500

    next js

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Aboutcomp=()=>{
+   return (
+    <>
+    
+  <div>
+    about page 
+  </div>
+  <Link href="/api">click to route</Link>
+    </>
+   )
+}
+export default Aboutcomp
